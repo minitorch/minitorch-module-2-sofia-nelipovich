f89137d7c73140c6bcd4f78db8137a5958e5edcc
Sofia Nelipovich <sofia.nelipovich@gmail.com>
2025-09-27 15:58:33 +0300

    fix tensor_ops.py

--- minitorch/tensor_ops.py
+++ minitorch/tensor_ops.py
@@ -26,21 +26,21 @@ class MapProto(Protocol):
 class TensorOps:
     @staticmethod
     def map(fn: Callable[[float], float]) -> MapProto:
-        pass  # type: ignore
+        raise NotImplementedError("Implement map in subclass")
 
     @staticmethod
     def cmap(fn: Callable[[float], float]) -> Callable[[Tensor, Tensor], Tensor]:
-        pass  # type: ignore
+        raise NotImplementedError("Implement cmap in subclass")
 
     @staticmethod
     def zip(fn: Callable[[float, float], float]) -> Callable[[Tensor, Tensor], Tensor]:
-        pass  # type: ignore
+        raise NotImplementedError("Implement zip in subclass")
 
     @staticmethod
     def reduce(
         fn: Callable[[float, float], float], start: float = 0.0
     ) -> Callable[[Tensor, int], Tensor]:
-        pass  # type: ignore
+        raise NotImplementedError("Implement reduce in subclass")
 
     @staticmethod
     def matrix_multiply(a: Tensor, b: Tensor) -> Tensor:
